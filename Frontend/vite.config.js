81b595154a462317400b7486780ceaabe752f6f5
BabakhalilM <154895694+BabakhalilM@users.noreply.github.com>
2024-07-23 18:18:34 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure minification is enabled
+    sourcemap: false
+  },
   server: {
     host: '0.0.0.0',
     port: 5173
